093b3bd35fcbb440e40e1ff8339338a6dc9ea1e8
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-02-25 17:16:37 +0530

    Player Movement Done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.up * speed * inputY * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * inputX * Time.deltaTime);
+
+        if (transform.position.y > 4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.5f, 0);
+        }
+
+        else if (transform.position.y < -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+
+        if (transform.position.x > 7f)
+        {
+            transform.position = new Vector3(7f, transform.position.y, 0);
+
+        }
+        else if (transform.position.x < -7f)
+        {
+            transform.position = new Vector3(-7f, transform.position.y, 0);
+        }
+
+    }
+}
