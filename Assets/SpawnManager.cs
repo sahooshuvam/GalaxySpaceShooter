d5641beb50bab7b4d640ab3904e44d879beb1e8b
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-02-28 14:38:38 +0530

    Instantiate Astroid Prefabs in a Random Position.

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject astroidPrefab;
+    public float time;
+   
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time>5f)
+        {
+            transform.position = new Vector3(8.25f, Random.Range(-4.5f, 4.5f));
+            Instantiate(astroidPrefab, transform.position, Quaternion.identity);
+            time = 0f;
+        }
+        
+    }
+}
